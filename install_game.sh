c11297281128ec260a1b1a3249e76ac4f9ee71d4
Jerickson Mayor <jericksonmayor@gmail.com>
2023-09-08 20:39:52 +0800

    Add install_game.sh script for game setup and installation

---
+++ install_game.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+
+# Check if java is installed
+if [ -z "$(which java)" ]; then
+	echo "Java is not installed. Please install Java first."
+	exit 1
+fi
+
+
+# Define the name of the target folder
+target_folder="$HOME/simple-maze-game"
+
+# Check if the target folder already exists
+if [ -d "$target_folder" ]; then
+    echo "The '$target_folder' folder already exists. Skipping the clone step."
+else
+    # Clone the repository
+    git clone https://github.com/elyeandre/simple-maze-game "$target_folder"
+    echo "Repository cloned successfully."
+fi
+
+# Navigate into the target folder
+cd "$target_folder"
+
+
+# Navigate into the bin folder
+cd bin
+
+# Create a JAR file named mygame.jar
+jar cvfm mygame.jar MANIFEST.MF .
+echo "JAR file 'mygame.jar' created successfully."
+
+# Run the game
+java -jar mygame.jar
+
+# Exit
+exit 0
+
